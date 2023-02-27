90c270fa76433b00dc44bc4da20775074b64ac86
bsstahl <barry.stahl@carvana.com>
2023-02-26 18:59:14 -0700

    Stub method and unit test. Both still have unimplemented features.

---
+++ src/PDM.Core/ByteExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace PDM;
+
+public static class ByteExtensions
+{
+    public static byte[] Map(this byte[] sourceMessage, string sourceProto, string targetProto, string transformation)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ tst/PDM.Core.Test/MessageExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Google.Protobuf;
+
+namespace PDM.Core.Test;
+
+public static class MessageExtensions
+{
+    public static byte[] ToByteArray(this IMessage message)
+    {
+        // TODO: Use expandable Stream rather than fixed length output
+        var flatArray = new byte[64];
+        CodedOutputStream stream = new CodedOutputStream(flatArray);
+        message.WriteTo(stream);
+        return flatArray;
+    }
+
+    public static T ToMessage<T>(this byte[] data)
+        where T : IMessage<T>
+    {
+        throw new NotImplementedException();
+    }
+}
