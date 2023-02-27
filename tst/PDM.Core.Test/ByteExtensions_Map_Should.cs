c49a40cbcea447a503c77313dd02488a2cacd2ee
Barry Stahl <bss_github@bsstahl.com>
2023-02-26 20:36:25 -0700

    ByteExtensions_Map_Should.ProperlyMapASubsetTypeToMatchingFields - Red

--- tst/PDM.Core.Test/ByteExtensions_Map_Should.cs
+++ tst/PDM.Core.Test/ByteExtensions_Map_Should.cs
@@ -1,3 +1,5 @@
+using Google.Protobuf.WellKnownTypes;
+
 namespace PDM.Core.Test;
 
 public class ByteExtensions_Map_Should
@@ -17,6 +19,29 @@ public class ByteExtensions_Map_Should
         var description = System.IO.File.ReadAllText(protoFilePath);
 
         var actual = sourceMessage.Map(description, description, string.Empty);
+        var actualData = ProtoBuf.TwoFields.Parser.ParseFrom(actual);
+
+        Assert.Equal(sourceData.IntegerValue, actualData.IntegerValue);
+    }
+
+    [Fact]
+    public void ProperlyMapASubsetTypeToMatchingFields()
+    {
+        var sourceData = new ProtoBuf.ThreeFields()
+        {
+            IntegerValue = Int32.MaxValue.GetRandom(),
+            FloatValue = Convert.ToSingle(double.MaxValue.GetRandom()),
+            StringValue = String.Empty.GetRandom()
+        };
+
+        var sourceMessage = sourceData.ToByteArray();
+
+        var sourceDescriptionFilePath = @"../../../ProtoBuf/ThreeFields.proto";
+        var sourceDescription = System.IO.File.ReadAllText(sourceDescriptionFilePath);
+        var targetDescriptionFilePath = @"../../../ProtoBuf/TwoFields.proto";
+        var targetDescription = System.IO.File.ReadAllText(targetDescriptionFilePath);
+
+        var actual = sourceMessage.Map(sourceDescription, targetDescription, string.Empty);
         var actualData = ProtoBuf.TwoFields.Parser.ParseFrom(actual);
 
         Assert.Equal(sourceData.IntegerValue, actualData.IntegerValue);
